698a91bf42aef1fdf547eb9bc557e954b9ba53ca
denis.trocenko <denis.v.trocenko@gmail.com>
2015-05-13 16:20:05 +0300

    Added: fonts, jqueryUI. New style.

---
+++ public/mock/participant.js
@@ -0,0 +1,347 @@
+const PARTICIPANT_MAIN_CLASS = 'participant main';
+const PARTICIPANT_CLASS = 'participant';
+
+
+/**
+ * Creates a video element for a new participant
+ *
+ * @param {String} name - the name of the new participant, to be used as tag
+ *                        name of the video element.
+ *                        The tag of the new element will be 'video<name>'
+ * @return
+ */
+function Participant(name, sendFunction, isLocalUser) {
+    var userId = name;
+	this.name = name;
+    this.sendFunction = sendFunction;
+    this.isLocalUser = isLocalUser;
+
+    var out = {};
+
+    if (!this.isLocalUser){
+        //var container = document.createElement('div');
+        //container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
+        //container.id = name;
+        //var span = document.createElement('span');
+        //var video = document.createElement('video');
+        ////video.setAttribute("style","width:200px");
+        //
+        //var stream;
+        //var rtcPeer;
+        //
+        //container.appendChild(video);
+        //container.appendChild(span);
+        //container.onclick = switchContainerClass;
+        //
+        //document.getElementById('participants').appendChild(container);
+        //
+        //span.appendChild(document.createTextNode(name));
+        //video.id = 'video-' + name;
+        //video.autoplay = true;
+        //video.controls = false;
+
+        var container = document.createElement('div');
+        var video = document.createElement('video');
+        container.appendChild(video);
+        var buttons = document.createElement('div');
+        container.appendChild(buttons);
+        var btnMute = document.createElement('button');
+        buttons.appendChild(btnMute);
+        var span = document.createElement('span');
+        btnMute.appendChild(span);
+        var sldVolume = document.createElement('input');
+        buttons.appendChild(sldVolume);
+        document.getElementById('participants').appendChild(container);
+
+        //container.className = "video-panel";
+        container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
+        container.id = name;
+        container.onclick = switchContainerClass;
+        video.id = 'video-' + name;
+        video.autoplay = true;
+        video.controls = false;
+        buttons.className = "btn-group video-control";
+        btnMute.type = "button";
+        btnMute.className = "btn btn-default btn-xs";
+        btnMute.id = "webcam-other-mute";
+        span.className = "glyphicon glyphicon-volume-off";
+        span.setAttribute("aria-hidden", "true");
+        sldVolume.className = "slider-volume";
+        sldVolume.id = "webcam-other-volume";
+        sldVolume.type = "range";
+        sldVolume.min="0";
+        sldVolume.max="100";
+        sldVolume.value="50";
+        sldVolume.step="1";
+        sldVolume.orient="vertical";
+
+        var stream;
+        var rtcPeer;
+    } else {
+        video = document.getElementById("webcam-me");
+        var container = video.parentNode;
+        video.id = 'video-' + name;
+        //video.autoplay = true;
+        video.controls = false;
+    }
+
+	//var container = document.createElement('div');
+	//container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
+	//container.id = name;
+	//var span = document.createElement('span');
+    //var video = document.createElement('video');
+    //video.setAttribute("style","width:200px");
+    //
+    //var stream;
+	//var rtcPeer;
+    //
+    //container.appendChild(video);
+    //container.appendChild(span);
+    //container.onclick = switchContainerClass;
+    //
+	//document.getElementById('participants').appendChild(container);
+    //
+	//span.appendChild(document.createTextNode(name));
+    //video.id = 'video-' + name;
+    //video.autoplay = true;
+    //
+	//video.controls = false;
+
+
+
+    // initialise a configuration for one stun server
+    var servers = {
+        iceServers: [
+            //{'url': 'stun:stun.l.google.com:19302'},
+            //{'url': 'stun:stun1.l.google.com:19302'},
+            //{'url': 'stun:stun2.l.google.com:19302'},
+            //{'url': 'stun:stun3.l.google.com:19302'},
+            //{'url': 'stun:stun4.l.google.com:19302'},
+            //{'url': 'stun:ekiga.net'},
+            //{'url': 'stun:ideasip.com'},
+            //{'url': 'stun:rixtelecom.se'},
+            //{'url': 'stun:schlund.de'}
+
+            {url: "stun:104.130.195.95"},
+            {url: "turn:104.130.198.83"},
+            {url: "turn:104.130.195.95:80?transport=tcp"},
+            {url: "turns:turn2.talky.io:443?transport=tcp"}
+
+            //"stun.l.google.com:19302",
+            //"stun1.l.google.com:19302",
+            //"stun2.l.google.com:19302",
+            //"stun3.l.google.com:19302",
+            //"stun4.l.google.com:19302",
+            //"stun.ekiga.net",
+            //"stun.ideasip.com",
+            //"stun.rixtelecom.se",
+            //"stun.schlund.de",
+            //"stun.stunprotocol.org:3478",
+            //"stun.voiparound.com",
+            //"stun.voipbuster.com",
+            //"stun.voipstunt.com",
+            //"stun.voxgratia.org",
+            //"stun.services.mozilla.com"
+        ],
+        optional: {
+            googIPv6:true,
+            googImprovedWifiBwe:true,
+            googDscp:true,
+            googSuspendBelowMinBitrate:true,
+            googScreencastMinBitrate:400,
+            andyetAssumeSetLocalSuccess:true,
+            andyetFirefoxMakesMeSad:500
+        }
+    };
+
+    var optional = {
+        optional: [
+            {DtlsSrtpKeyAgreement: true},
+            {googDscp: true},
+            {andyetAssumeSetLocalSuccess: true},
+            {andyetFirefoxMakesMeSad:500}
+        ]
+    };
+
+
+    this.start = function(handler) {
+        this.handler = handler;
+        console.log("Participant:start " + userId + " isLocal:" + this.isLocalUser);
+        if (this.isLocalUser) {
+            getUserMedia({
+                    audio: true,
+                    video: true
+                }, this.gotLocalStream.bind(this),
+                function(e) {
+                    alert('getUserMedia() error: ' + e.name);
+                });
+        } else {
+            rtcPeer = new RTCPeerConnection(servers, optional);
+            rtcPeer.onicecandidate = function(e) {
+                console.log("onicecandidate " + e.candidate);
+                if (e.candidate) {
+                    sendFunction(userId, {method:"addIceCandidate", broadcastId:userId, candidate: e.candidate});
+                }
+            };
+            rtcPeer.onaddstream = gotRemoteStream;
+            this.sendFunction(userId, {method:"requestCreateOffer", data:null, broadcastId:userId});
+        }
+    }
+
+    this.gotLocalStream = function(stream) {
+        trace('Received local stream');
+        // Call the polyfill wrapper to attach the media stream to this element.
+        attachMediaStream(video, stream);
+        this.stream = stream;
+        console.log(stream);
+        var videoTracks = stream.getVideoTracks();
+        var audioTracks = stream.getAudioTracks();
+        if (videoTracks.length > 0) {
+            trace('Using video device: ' + videoTracks[0].label);
+        }
+        if (audioTracks.length > 0) {
+            trace('Using audio device: ' + audioTracks[0].label);
+        }
+        this.handler();
+    }
+
+	function switchContainerClass() {
+		if (container.className === PARTICIPANT_CLASS) {
+			var elements = Array.prototype.slice.call(document.getElementsByClassName(PARTICIPANT_MAIN_CLASS));
+			elements.forEach(function(item) {
+					item.className = PARTICIPANT_CLASS;
+				});
+
+				container.className = PARTICIPANT_MAIN_CLASS;
+			} else {
+			container.className = PARTICIPANT_CLASS;
+		}
+	}
+
+	function isPresentMainParticipant() {
+		return ((document.getElementsByClassName(PARTICIPANT_MAIN_CLASS)).length != 0);
+	}
+
+	Object.defineProperty(this, 'rtcPeer', { writable: true});
+
+	this.dispose = function() {
+		console.log('Disposing participant ' + this.name);
+		//container.parentNode.removeChild(container);
+
+        if (this.isLocalUser && this.stream) {
+            video.src = "";
+            this.stream.stop();
+            video.id = "webcam-me";
+        } else {
+            container.parentNode.removeChild(container);
+        }
+        try {
+            for (var remoteUserId in out) {
+                out[remoteUserId].close();
+            }
+            if (rtcPeer) {
+                rtcPeer.close();
+            }
+        } catch (e) {
+            console.error("Wired thing in rtc peer dispose");
+        }
+    };
+
+    this.requestCreateOffer = function(remoteUserId, value) {
+        var pc = new RTCPeerConnection(servers, optional);
+        out[remoteUserId] = pc;
+        pc.addStream(this.stream);
+        pc.onicecandidate = function(e) {
+            console.log("onicecandidate " + e.candidate);
+            if (e.candidate) {
+                sendFunction(remoteUserId, {method:"addIceCandidate", broadcastId:userId, candidate: e.candidate});
+            }
+        };
+        pc.onnegotiationneeded = function () {
+            console.log("onnegotiationneeded");
+            pc.createOffer(
+                function(desc) {
+                    trace('Created offer\n' + 'desc.sdp');
+                    pc.setLocalDescription(desc, function() {});
+                    sendFunction(remoteUserId, {method:"respondCreateOffer", broadcastId:userId, desc:desc});
+                    console.log("createOffer success");
+                },
+                onCreateSessionDescriptionError
+            );
+        }
+        console.log("requestCreateOffer");
+    }
+
+    this.respondCreateOffer = function(remoteUserId, value) {
+        var desc = fixDesc(value.desc);
+        var sdpConstraints = {
+            'mandatory': {
+                'OfferToReceiveAudio': true,
+                'OfferToReceiveVideo': true
+            }
+        };
+
+        rtcPeer.setRemoteDescription(desc, function() {
+            console.log('setRemoteDescription success');
+            rtcPeer.createAnswer(
+                function onCreateAnswerSuccess(desc) {
+                    console.log('Created answer:\n' + 'desc.sdp');
+                    rtcPeer.setLocalDescription(desc,
+                        function() {
+                            console.log('Set local description from answer.');
+                        },
+                        onSetDescriptionError);
+                    sendFunction(userId, {method:"respondAnswer", broadcastId:userId, desc:desc});
+                },
+                onCreateSessionDescriptionError,
+                sdpConstraints
+            );
+        }, onSetDescriptionError);
+
+    }
+
+    this.addIceCandidate = function(remoteUserId, value) {
+        var candidate = new RTCIceCandidate(value.candidate);
+
+        if (this.isLocalUser) {
+            var pc = out[remoteUserId];
+            pc.addIceCandidate(candidate)
+        } else {
+            rtcPeer.addIceCandidate(candidate);
+        }
+    }
+
+    this.respondAnswer = function(remoteUserId, value) {
+        var desc = fixDesc(value.desc);
+        var pc = out[remoteUserId];
+
+        pc.setRemoteDescription(desc,
+            function() {
+                console.log("Set remote description from answer success")
+            },
+            onSetDescriptionError);
+        console.log("respondAnswer")
+    }
+
+    // keep correct object type after serialization
+    function fixDesc(value) {
+        var result = new RTCSessionDescription();
+        result.sdp = value.sdp;
+        result.type = value.type;
+        return result;
+    }
+
+    function gotRemoteStream(e) {
+        // Call the polyfill wrapper to attach the media stream to this element.
+        attachMediaStream(video, e.stream);
+        trace("rtcPeer received remote stream");
+    }
+
+    function onCreateSessionDescriptionError(error) {
+        trace("Failed to create session description: " + error.message);
+    }
+
+    function onSetDescriptionError(error) {
+        trace("Failed to set description: " + error.message);
+    }
+}
---
+++ public/mock/room.js
@@ -0,0 +1,301 @@
+(function () {
+
+    window.onload = function() {
+        console.log("Page loaded ...");
+        //console = new Console('console', console);
+        $("#sharing-image").draggable();
+    }
+
+    function removeError() {
+        $('#error').fadeOut(500);
+    }
+
+    function setError(message) {
+        $('#error').empty().append($('<span class="error" />').text(message)).fadeIn(500);
+    }
+
+    if (!window.WebSocket) {
+        if (window.MozWebSocket)
+            window.WebSocket = window.MozWebSocket
+    }
+
+    if (!window.WebSocket) {
+        setError("WebSocket is not supported by your browser.");
+        return;
+    }
+
+    //$('#participants').css("width", 200 + "px");
+
+    var room = "default";
+    var participants = {};
+    var name;
+    var broadcasting = false;
+
+    var insideIframe = (window.parent != window);
+    var isMobile = /ipad|iphone|android/i.test(navigator.userAgent);
+
+    // STATES
+    // Key value store of data
+    var roomState = {};    // persisted state: shared content, chat
+
+    var pid;
+    var pname;
+
+    var onSocketMessage;
+
+    // Init pname
+    function queryPname() {
+        var n = prompt("What is your name?");
+        if (n) {
+            localStorage.setItem("pname", n);
+        }
+        return n || pname;
+    }
+
+    pname = localStorage.getItem("pname");
+    if (!insideIframe && !pname) {
+        pname = queryPname();
+    }
+    if (!pname) {
+        pname = "User " + Math.floor(100 * Math.random());
+    }
+    setInterval(function() {
+        if (connected) {
+            send({messageType: "ping"});
+        }
+    }, 5000);
+
+
+
+    function sendCommand(action) {
+        var commandData = $("#commandInput").val();
+        var data = action || commandData;
+        if (data) {
+            $("#commandInput").val("");
+            send({messageType: "command", data: data});
+        }
+    }
+
+    // send modify state message
+    function sendChangeMessage(key, value) {
+        send({messageType: "change", key: key, value: value});
+    }
+
+    // send message to specific user
+    function sendToMessage(toUserId, value) {
+        send({messageType: "sendTo", toUserId: toUserId, fromUserId: pid, value: value});
+    }
+
+    function receiveVideo(remoteUserId) {
+        console.log("receiveVideo remoteUserId:" + remoteUserId)
+        var participant = new Participant(remoteUserId, sendToMessage, false);
+        participants[remoteUserId] = participant;
+        participant.start();
+    }
+
+    function sendChatMessage() {
+        sendChangeMessage("chat." + new Date().getTime(), "<strong>" + pname + ":</strong> " + $("#commandInput").val());
+        $("#commandInput").val("");
+    }
+
+    $('#pname').text(pname).click(function (e) {
+        e.preventDefault();
+        pname = queryPname();
+        send({messageType: "changeName", name: pname});
+        $('#pname').text(pname);
+    });
+
+    $('#commandSendButton').on('click', function (e) {
+        $("#commandInput").trigger("change");
+        //sendChatMessage();
+    }, false);
+
+    $("#commandInput").change(function(){
+        sendChatMessage();
+    });
+
+    $('#commandClearButton').on('click', function (e) {
+        sendCommand("clear");
+    });
+
+    // start or stop local broadcast
+    $('#commandPlayButton').on('click', function (e) {
+        var participant = participants[pid];
+        if (broadcasting) {
+            broadcasting = false;
+            //$('#commandPlayButton').text(broadcasting ? "Stop Broadcast" : "Broadcast");
+            //$('#commandPlayButton').css({"background-color": (broadcasting ? "#0C0" : "#C00"), "border-color": (broadcasting ? "#0C0" : "#C00")});
+            $('#commandPlayButton').css({"background-color": "#C00", "border-color": "#C00"});
+            // remove local broadcast object from state
+            sendChangeMessage("broadcast." + pid, null);
+
+            if (participant) {
+                delete participants[pid];
+                participant.dispose();
+                console.log("Removing local broadcast")
+            }
+        } else {
+            console.log(pid + " registered in room " + room);
+            if (participant) {
+                participant.dispose();
+            }
+            participant = new Participant(pid, sendToMessage, true);
+            participants[pid] = participant;
+            participant.start(onBroadcastReady);
+            //$('#commandPlayButton').text("Starting...");
+        }
+    });
+
+    function onBroadcastReady() {
+        broadcasting = true;
+        //$('#commandPlayButton').text(broadcasting ? "Stop Broadcast" : "Broadcast");
+        //$('#commandPlayButton').css({"background-color": (broadcasting ? "#0C0" : "#C00"), "border-color": (broadcasting ? "#0C0" : "#C00")});
+        $('#commandPlayButton').css({"background-color": "#0C0", "border-color": "#0C0"});
+        // add local broadcast object to state
+        sendChangeMessage("broadcast." + pid, true);
+    }
+
+    //$('#commandForm').submit(function (event) {
+    //    // prevent default browser behaviour
+    //    event.preventDefault();
+    //
+    //    sendChatMessage();
+    //});
+
+
+
+    // WebSocket
+    var socket;
+    var connected = false;
+
+    var reconnectInterval = 1000;
+    var reconnection = false;
+
+    function tryConnectAgain() {
+        reconnection = true;
+        setError("Reconnecting...");
+        setTimeout(function () {
+            reconnectInterval *= (1.5 + 0.2 * Math.random());
+            connect();
+        }, reconnectInterval);
+    }
+
+    function connect() {
+        try {
+            var pathArray = window.location.pathname.split( '/' );
+            if (window.location.pathname.length > 1 && pathArray.length >= 2) {
+                room = pathArray[pathArray.length - 1];
+            }
+            var url = "ws://" + location.host + "/stream/" + room
+            console.log("Connecting to " + url + " from " + window.location.pathname);
+            socket = new WebSocket(url);
+            socket.onmessage = onSocketMessage;
+            socket.onopen = function (evt) {
+                if (reconnection) {
+                    window.location = window.location; // Reloading the page to reset states
+                    return;
+                }
+                connected = true;
+                console.log("websocket on open")
+                send({messageType: "join", name: pname});
+            };
+            socket.onclose = function (evt) {
+                connected = false;
+                tryConnectAgain();
+            };
+            socket.onerror = function (evt) {
+                console.error("error", evt);
+            };
+        }
+        catch (e) {
+            setError("WebSocket connection failed.");
+        }
+    }
+
+    function send(o) {
+        if (!connected) return;
+        socket.send(JSON.stringify(o));
+    }
+
+    function doChangeBroadcast(remoteUserId, value) {
+        if (remoteUserId != pid) {
+            var existingBroadcast = participants[remoteUserId];
+            if (existingBroadcast && !value) {
+                existingBroadcast.dispose();
+                delete participants[remoteUserId];
+            } else if (!existingBroadcast && value) {
+                receiveVideo(remoteUserId);
+            }
+        }
+    }
+
+    function doSendTo(fromUserId, value) {
+        var participant = participants[value.broadcastId];
+        if (participant) {
+            participant[value.method](fromUserId, value);
+        }
+    }
+
+
+    (function () {
+
+        onSocketMessage = function (e) {
+            var m = JSON.parse(e.data);
+            console.log("onSocketMessage " + m.messageType + " " + e.data.substr(0, 100))
+            if (m.messageType == "youAre") {
+                pid = m.pid;
+                console.log("Set pid " + pid)
+                $("#localTextArea").html("Your id is " + pid + " in " + room);
+                //$("#pid").html("Id: " + pid);
+            } else if (m.messageType == "change") {
+                if (m.bracket == "user") {
+                    console.log("user change " + m.id + " " + m.value)
+                    var userId = m.id;
+                    var participant = participants[userId];
+                    if (m.value == null) {
+                        // delete user
+                        if (participant) {
+                            delete participants[userId];
+                            participant.dispose();
+                        }
+                    } else {
+                        if (!participant) {
+                            participants[userId] = participant;
+                            if (pid != userId) {    // don't subscribe own video
+                                //receiveVideo(userId)
+                            }
+                        }
+                    }
+                } else if (m.key.indexOf("broadcast.") == 0) {
+                    var broadcastUserId = m.key.split(".")[1];
+                    doChangeBroadcast(broadcastUserId, m.value);
+                } else if (m.key.indexOf("chat.") == 0) {
+                    var chatArea = $("#chatArea");
+                    chatArea.html(chatArea.html() + "<p>" + m.value + "</p>")
+                    chatArea.get(0).scrollTop = chatArea.get(0).scrollHeight;
+                    console.log("Append child " + m.value)
+                }
+            } else if (m.messageType == "chatClear") {
+                $("#chatArea").html("");
+            } else if (m.messageType == "sendTo") {
+                doSendTo(m.fromUserId, m.value);
+            } else if (m.messageType == "chatMessage") {
+                var chatArea = $("#chatArea");
+                chatArea.html(chatArea.html() + "<p><strong>" + m.name + ": </strong>" + m.message + "</p>")
+                chatArea.get(0).scrollTop = chatArea.get(0).scrollHeight;
+                console.log("Append child " + m.message)
+            } else if (m.messageType == "status") {
+                $("#localTextArea").html(m.local);
+                $("#allTextArea").html(m.all);
+            } else if (m.messageType == "sdpAnswerMessage") {
+                var sdpAnswer = m.sdpAnswer;
+                var userId = m.id;
+
+                console.log("sdpAnswerMessage received " + userId)
+                participants[userId].rtcPeer.processSdpAnswer(sdpAnswer);
+            }
+        }
+    }());
+
+    connect();
+}());
